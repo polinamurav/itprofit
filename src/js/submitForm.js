fa7188219f1d9409c92b86cca3bb1ba0e732064a
Polina Muravieva <polinamur3@gmail.com>
2024-10-30 04:00:56 +0300

    handle server responses

--- src/js/submitForm.js
+++ src/js/submitForm.js
@@ -3,10 +3,11 @@ import {CustomHttp} from "../services/custom-http";
 import config from "../config/config";
 
 export class SubmitForm {
-    constructor() {
+    constructor(form) {
         document.getElementById('button').addEventListener('click', this.sendMessage.bind(this));
 
         this.validation = new Validation();
+        this.form = document.getElementById('form');
         this.loader = document.getElementsByClassName('loader-main')[0];
     }
 
@@ -17,7 +18,6 @@ export class SubmitForm {
         document.querySelectorAll('.error-input').forEach(element => {
             element.style.display = 'none';
         })
-
 
         if (this.validation.validateForm()) {
             const createData = {
@@ -31,9 +31,39 @@ export class SubmitForm {
 
             if (response.status === 'success') {
                 alert(response.msg);
+                this.form.reset();
             } else if (response.status === 'error') {
+                this.displayErrors(response.fields);
             }
         }
         this.loader.style.display = 'none';
+    }
+
+    displayErrors(fields) {
+        this.textInputArray = [this.validation.nameInputElement, this.validation.phoneInputElement,
+            this.validation.messageInputElement, this.validation.emailInputElement];
+        this.textInputArray.forEach(input => {
+            const errorMessageElement = input.nextElementSibling;
+            if (errorMessageElement) {
+                errorMessageElement.style.display = 'none';
+                input.classList.remove('error');
+                input.style.borderColor = '';
+            }
+        });
+
+        for (const field in fields) {
+            const input = this.textInputArray.find(input => input.name === field);
+            const errorMessageElement = input ? input.nextElementSibling : null;
+
+            if (input) {
+                input.classList.add('error');
+                input.style.borderColor = 'red';
+            }
+
+            if (errorMessageElement) {
+                errorMessageElement.style.display = 'block';
+                errorMessageElement.innerText = fields[field];
+            }
+        }
     }
 }
